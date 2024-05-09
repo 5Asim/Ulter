d553855523045aacbe823661d446b1d2baaa431f
5Asim <ashim.shrestha.55@gmail.com>
2024-05-09 12:01:33 +0545

    camera updated

--- Frontend/src/components/camera/CameraComponent.tsx
+++ Frontend/src/components/camera/CameraComponent.tsx
@@ -1,10 +1,9 @@
 import { useRef, useState, useEffect } from 'react';
 
 const CameraComponent = () => {
-    // Specify the type of elements these refs will hold
     const videoRef = useRef<HTMLVideoElement>(null);
     const canvasRef = useRef<HTMLCanvasElement>(null);
-    const [image, setImage] = useState<string>(''); // Explicitly type the state as well
+    const [image, setImage] = useState<string>('');
     const [showCamera, setShowCamera] = useState<boolean>(false);
 
     useEffect(() => {
@@ -13,7 +12,14 @@ const CameraComponent = () => {
 
     const startCamera = async () => {
         try {
-            const stream = await navigator.mediaDevices.getUserMedia({ video: { width: 1280, height: 720 } });
+            const constraints = {
+                video: {
+                    width: { ideal: 1280 }, // You can specify 'ideal' for better device compatibility
+                    height: { ideal: 720 },
+                    facingMode: "environment" // This should select the back camera on most devices
+                }
+            };
+            const stream = await navigator.mediaDevices.getUserMedia(constraints);
             if (videoRef.current) videoRef.current.srcObject = stream;
         } catch (error) {
             console.error('Error accessing the camera:', error);
@@ -23,14 +29,11 @@ const CameraComponent = () => {
     const takePicture = () => {
         if (videoRef.current && canvasRef.current) {
             const context = canvasRef.current.getContext('2d');
-            if (context) { // Always good to check for nullability
+            if (context) {
                 const { videoWidth, videoHeight } = videoRef.current;
-
                 canvasRef.current.width = videoWidth;
                 canvasRef.current.height = videoHeight;
-
                 context.drawImage(videoRef.current, 0, 0, videoWidth, videoHeight);
-
                 const imageDataUrl = canvasRef.current.toDataURL('image/png');
                 setImage(imageDataUrl);
             }
@@ -43,7 +46,7 @@ const CameraComponent = () => {
 
     return (
         <div>
-            <video ref={videoRef} autoPlay playsInline style={{ width: '100%' }}></video>
+            <video ref={videoRef} autoPlay playsInline style={{ width: '100vw', height: '100vh', objectFit: 'cover' }}></video>
             <button onClick={startCamera}>Start Camera</button>
             <button onClick={takePicture}>Take Picture</button>
             <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
